95120e0cbdc1b48ca938b03bc30078d3410fc84f
Nikita Popelnukh <factionsinsanity@gmail.com>
2021-11-10 13:44:26 -0500

    OOP and wall detections
    Reformatted all of the code in OOP and now when you create a wall it removes that available node from the nodes array, making it unavailable to move to.
    
    Game is pretty much finished. Will be working on the algorithm next.

--- game.py
+++ game.py
@@ -2,81 +2,106 @@ import sys
 import pygame
 from pygame import event
 
-w = 1400
-h = 800
-
 GRAY = (100, 100, 100)
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 
-nodesx = []
-nodesy = []
-startnode = []
-endnode = []
-wall = []
-neighbors = []
-move = []
 
-display = pygame.display.set_mode((w, h))
-pygame.init()
-surface = pygame.display.set_mode((w, h))
+class Game:
+    def __init__(self, w=1400, h=800, nodeSize=20):
+        self.display = pygame.display.set_mode((w, h))
+        pygame.init()
+        self.surface = pygame.display.set_mode((w, h))
+        self.nodeSize = nodeSize
+        self.surface.fill((2, 0, 50))
 
-surface.fill((2, 0, 50))
+        self.w = w
+        self.h = h
+        self.nodes = []
+        self.startnode = []
+        self.endnode = []
+        self.walls = []
+        self.neighbors = []
+        self.move = []
 
-nodeSize = 20
-for x in range(0, w, nodeSize):  # Loops through the GUI width in steps of 20 and appends the values in the nodesx array
-    for y in range(0, h, nodeSize):  # Same as above but for y, appends in nodesy array
-        node = pygame.Rect(x, y, nodeSize, nodeSize)
+    def drawgrid(self):
+        for x in range(0, self.w,
+                       self.nodeSize):  # Loops through the GUI width in steps of 20 and appends the values in the nodesx array
+            for y in range(0, self.h, self.nodeSize):  # Same as above but for y, appends in nodesy array
+                node = pygame.Rect(x, y, self.nodeSize, self.nodeSize)
 
-        pygame.draw.rect(surface, BLACK, node, 1)  # Draws the grid
-        nodesx.append(x)
-        nodesy.append(y)
+                pygame.draw.rect(self.surface, BLACK, node, 1)  # Draws the grid
+                self.nodes.append((x, y))
 
-while True:
-    pygame.display.flip()
-    for event in pygame.event.get():
-        if pygame.mouse.get_pressed()[0]:
-            pos = pygame.mouse.get_pos()
-            for x, y in zip(nodesx, nodesy):
-                if x - 0 < pos[0] < x + 20 and y - 0 < pos[1] < y + 20:
-                    startnode.append(x)
-                    startnode.append(y)
-                    if len(startnode) == 2:
-                        node2 = pygame.Rect(x, y, nodeSize, nodeSize)
-                        pygame.draw.rect(surface, GREEN, node2, 1)
-                    elif len(startnode) > 2:
-                        node2 = pygame.Rect(startnode[0], startnode[1], nodeSize, nodeSize)
-                        pygame.draw.rect(surface, BLACK, node2, 1)
-                        startnode.clear()
-                        neighbors.clear()
+    def game(self):
+        global event
+        while True:
+            pygame.display.flip()
+            for event in pygame.event.get():
+                if pygame.mouse.get_pressed()[0]:
+                    pos = pygame.mouse.get_pos()
+                    for x, y in self.nodes:
+                        if x - 0 < pos[0] < x + 20 and y - 0 < pos[1] < y + 20:
+                            self.startnode.append((x, y))
 
-        elif pygame.mouse.get_pressed()[2]:
-            pos = pygame.mouse.get_pos()
-            for x, y in zip(nodesx, nodesy):
-                if x - 0 < pos[0] < x + 20 and y - 0 < pos[1] < y + 20:
-                    endnode.append(x)
-                    endnode.append(y)
-                    print(f"Endnode: {endnode}. Startnode {startnode}.")
-                    print("Distance: " + str((endnode[0]-startnode[0])/20))
-                    if len(endnode) == 2:
-                        node2 = pygame.Rect(x, y, nodeSize, nodeSize)
-                        pygame.draw.rect(surface, RED, node2, 1)
-                    elif len(endnode) > 2:
-                        node2 = pygame.Rect(endnode[0], endnode[1], nodeSize, nodeSize)
-                        pygame.draw.rect(surface, BLACK, node2, 1)
-                        endnode.clear()
+                            if len(self.startnode) == 1:
+                                node2 = pygame.Rect(x, y, self.nodeSize, self.nodeSize)
+                                pygame.draw.rect(self.surface, GREEN, node2, 1)
 
-        if pygame.mouse.get_pressed()[1]:
-            pos = pygame.mouse.get_pos()
-            for x, y in zip(nodesx, nodesy):
-                if x - 0 < pos[0] < x + 20 and y - 0 < pos[1] < y + 20:
-                    wall.append(x)
-                    wall.append(y)
-                    node2 = pygame.Rect(x, y, nodeSize, nodeSize)
-                    pygame.draw.rect(surface, WHITE, node2, 3)
+                            elif len(self.startnode) > 1:
+                                node2 = pygame.Rect(self.startnode[0][0], self.startnode[0][1], self.nodeSize,
+                                                    self.nodeSize)
+                                pygame.draw.rect(self.surface, BLACK, node2, 1)
+                                self.startnode.clear()
+                                self.neighbors.clear()
 
-    if event.type == pygame.QUIT:
-        pygame.quit()
-        sys.exit()
+                elif pygame.mouse.get_pressed()[2]:
+                    pos = pygame.mouse.get_pos()
+                    for x, y in self.nodes:
+                        if x - 0 < pos[0] < x + 20 and y - 0 < pos[1] < y + 20:
+                            self.endnode.append((x, y))
+                            print(f"Endnode: {self.endnode}. Startnode {self.startnode}.")
+                            print("Distance: " + str((self.endnode[0][0] - self.startnode[0][1]) / 20))
+
+                            if len(self.endnode) == 1:
+                                node2 = pygame.Rect(x, y, self.nodeSize, self.nodeSize)
+                                pygame.draw.rect(self.surface, RED, node2, 1)
+
+                            elif len(self.endnode) > 1:
+                                node2 = pygame.Rect(self.endnode[0][0], self.endnode[0][1], self.nodeSize,
+                                                    self.nodeSize)
+                                pygame.draw.rect(self.surface, BLACK, node2, 1)
+                                self.endnode.clear()
+
+                if pygame.mouse.get_pressed()[1]:
+                    pos = pygame.mouse.get_pos()
+                    for x, y in self.nodes:
+                        if x - 0 < pos[0] < x + 20 and y - 0 < pos[1] < y + 20:
+                            self.walls.append((x, y))
+                            node2 = pygame.Rect(x, y, self.nodeSize, self.nodeSize)
+                            pygame.draw.rect(self.surface, WHITE, node2, 3)
+                            print(self.nodes)
+
+                    for wall in self.walls:
+                        for i, node in enumerate(self.nodes):
+                            if wall == node:
+                                self.nodes.pop(i)
+
+
+
+
+
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.drawgrid()
+    game.game()
