627462ee1567d3cee298846040e5800ce0c08e9d
Nikita Popelnukh <factionsinsanity@gmail.com>
2021-11-11 12:00:30 -0500

    Update
    Algorithm is half finished, game is finished completely.
    
    There are currently a lot of bugs, I will iron those out later on.

--- game.py
+++ game.py
@@ -1,10 +1,13 @@
 import sys
 import pygame
 from pygame import event
+import time
+import numpy as np
 
 GRAY = (100, 100, 100)
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 
@@ -34,6 +37,7 @@ class Game:
 
                 pygame.draw.rect(self.surface, BLACK, node, 1)  # Draws the grid
                 self.nodes.append((x, y))
+                # pygame.display.flip()
 
     def game(self):
         global event
@@ -45,10 +49,12 @@ class Game:
                     for x, y in self.nodes:
                         if x - 0 < pos[0] < x + 20 and y - 0 < pos[1] < y + 20:
                             self.startnode.append((x, y))
+                            self.move.append((x, y))
 
                             if len(self.startnode) == 1:
                                 node2 = pygame.Rect(x, y, self.nodeSize, self.nodeSize)
                                 pygame.draw.rect(self.surface, GREEN, node2, 1)
+                                game.algorithm()
 
                             elif len(self.startnode) > 1:
                                 node2 = pygame.Rect(self.startnode[0][0], self.startnode[0][1], self.nodeSize,
@@ -63,7 +69,7 @@ class Game:
                         if x - 0 < pos[0] < x + 20 and y - 0 < pos[1] < y + 20:
                             self.endnode.append((x, y))
                             print(f"Endnode: {self.endnode}. Startnode {self.startnode}.")
-                            print("Distance: " + str((self.endnode[0][0] - self.startnode[0][1]) / 20))
+                            # print("Distance: " + str((self.endnode[0][0] - self.startnode[0][1]) / 20))
 
                             if len(self.endnode) == 1:
                                 node2 = pygame.Rect(x, y, self.nodeSize, self.nodeSize)
@@ -82,23 +88,62 @@ class Game:
                             self.walls.append((x, y))
                             node2 = pygame.Rect(x, y, self.nodeSize, self.nodeSize)
                             pygame.draw.rect(self.surface, WHITE, node2, 3)
-                            print(self.nodes)
 
-                    for wall in self.walls:
-                        for i, node in enumerate(self.nodes):
-                            if wall == node:
-                                self.nodes.pop(i)
-
-
-
-
-
+                    # for wall in self.walls:
+                    #   for i, node in enumerate(self.nodes):
+                    #       if wall == node:
+                    #           self.nodes.pop(i)
 
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
+    def algorithm(self):
+        while self.move != self.endnode:
+            # self.nodes.pop(2)
+            start = self.nodes.index(self.startnode[0])
+            test = self.nodes.index(self.move[0])
+            end = self.nodes.index(self.endnode[0])
 
+            up = self.nodes[test - 1]
+            down = self.nodes[test + 1]
+            left = self.nodes[test + 40]
+            right = self.nodes[test - 40]
+
+            nodeup = pygame.Rect(up[0], up[1], self.nodeSize, self.nodeSize)
+            nodedown = pygame.Rect(down[0], down[1], self.nodeSize, self.nodeSize)
+            noderight = pygame.Rect(right[0], right[1], self.nodeSize, self.nodeSize)
+            nodeleft = pygame.Rect(left[0], left[1], self.nodeSize, self.nodeSize)
+
+            pygame.draw.rect(self.surface, BLUE, noderight, 1)
+            pygame.draw.rect(self.surface, BLUE, nodeleft, 1)
+            pygame.draw.rect(self.surface, BLUE, nodeup, 1)
+            pygame.draw.rect(self.surface, BLUE, nodedown, 1)
+
+            hcostup = abs((test - 1) - end) #+ ((test - 1) - start))  # Need to give the distance to endnode
+            hcostdown = abs((test + 1) - end) #+ ((test + 1) - start))
+            hcostleft = abs((test + 40) - end) #+ ((test + 40) - start))
+            hcostright = abs((test - 40) - end) #+ ((test - 40) - start))
+
+            self.neighbors.clear()
+            self.move.clear()
+
+            self.neighbors.append((hcostup, hcostdown, hcostleft, hcostright))
+            print(self.neighbors)
+            if np.amin(self.neighbors) == self.neighbors[0][0]:
+                self.move.append(up)
+            elif np.amin(self.neighbors) == self.neighbors[0][1]:
+                self.move.append(down)
+            elif np.amin(self.neighbors) == self.neighbors[0][2]:
+                self.move.append(left)
+            elif np.amin(self.neighbors) == self.neighbors[0][3]:
+                self.move.append(right)
+
+            nextmove = pygame.Rect(self.move[0][0], self.move[0][1], self.nodeSize, self.nodeSize)
+            pygame.draw.rect(self.surface, RED, nextmove, 1)
+            pygame.display.flip()
+
+            time.sleep(0.01)
 
 
 if __name__ == "__main__":
